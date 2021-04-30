1673ac87907b57ec2674f092321a6111d3275879
Paul Williams <paul@paulswilliams.me.uk>
2021-04-30 15:16:06 +0100

    `clojure -X:new :name uk.me.paulswilliams/stripexif`

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to stripexif
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/uk/me/paulswilliams/stripexif_test.clj
@@ -0,0 +1,7 @@
+(ns uk.me.paulswilliams.stripexif-test
+  (:require [clojure.test :refer :all]
+            [uk.me.paulswilliams.stripexif :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
